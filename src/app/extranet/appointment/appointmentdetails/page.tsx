5febee9827919728753c2532aecf45b77ec0fcd0
fjhus16 <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-02 23:35:07 +0200

    init

---
+++ src/app/extranet/appointment/appointmentdetails/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Appointmment Details</h1>
+    </div>
+  );
+}
---
+++ src/app/extranet/appointment/setappointment/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Set Appointmment</h1>
+    </div>
+  );
+};
